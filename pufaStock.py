abcb24f2967cf4cd48ec92f80cc8e92ee199bc94
liuyun_ucas@163.com <MCefkNVe96k6TU>
2020-08-03 23:09:41 +0800

    把标准误差mae替换成标准误差百分比mape。mape低于1%时才具备参考性。

--- pufaStock.py
+++ pufaStock.py
@@ -26,7 +26,7 @@ def build_model(allDataShape):
     model.add(layers.Dense(64, activation='relu'))
     model.add(layers.Dense(1))
     #model.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics=['acc'])
-    model.compile(optimizer='rmsprop', loss='mse', metrics=['mae'])
+    model.compile(optimizer='rmsprop', loss='mae', metrics=['mape','mse'])
     return model
 
 if __name__ == '__main__':
@@ -39,7 +39,7 @@ if __name__ == '__main__':
 
     #下面是ZhaoYing修改添加训练数据的地方----------------------------------------------------------------------------------
     #需要指定数据里的最新时间
-    new_time = np.datetime64('2020-07-31')
+    new_time = np.datetime64('2020-08-03')
     #指定用于训练的列名
     col_data = ['市盈率', '市盈率(TTM)', '市净率', '开盘价', '最高价', '最低价', '前收盘价', '涨跌幅', '振幅', '换手率', '指数成分上涨数量', '指数成分下跌数量',
                 '近期创阶段新高', '近期创阶段新低', '连涨天数', '连跌天数', '向上有效突破5日均线', '向下有效突破5日均线', '向上有效突破10日均线', '向下有效突破10日均线',
@@ -47,9 +47,9 @@ if __name__ == '__main__':
                 'DMA平均线差', 'MACD', 'TRIX三重指数平滑平均', 'KDJ', 'RSI', 'VROC量变动速率', 'ARBR人气意愿指标', 'PSY心理指标', 'VR成交量比率',
                 'MFI资金流向指标', '多空布林线', '量比']
     #训练的轮数，先用1轮来跑通程序，然后改成10，50，100甚至更多来让训练更准确（也更慢）
-    num_epochs = 1
+    num_epochs = 100
     #预测的几天后的数据
-    predict_day = 2
+    predict_day = 1
     #上面是ZhaoYing修改添加训练数据的地方----------------------------------------------------------------------------------
 
 
@@ -66,7 +66,7 @@ if __name__ == '__main__':
     model = build_model(allDataShape)
 
     #使用KData.py里的K-折线方法切割数据为多组训练数据和测试数据
-    k = 4
+    k = 10
     k_partial_train_data,k_partial_train_targets,k_val_data,k_val_targets = getKData(all_data,all_targets,k)
 
     #开始训练
@@ -83,22 +83,25 @@ if __name__ == '__main__':
 
         history = model.fit(partial_train_data, partial_train_targets, epochs=num_epochs, batch_size=16, verbose=0,
                             validation_data=(val_data,val_targets))
-        test_mse_score, test_mae_score = model.evaluate(val_data, val_targets)
+        test_mae_score, test_mape_score,test_mse_score = model.evaluate(val_data, val_targets)
         print("历史预测误差MAE：" + str(test_mae_score))
+        print("历史预测误差百分比MAPE：" + str(test_mape_score))
         print("历史预测均方差MSE：" + str(test_mse_score))
-        mae_history = history.history['mae']
+        mae_history = history.history['mape']
         all_mae_histories.append(mae_history)
-        all_scores.append(test_mae_score)
+        all_scores.append(test_mape_score)
         #break
         res = model.predict(predict_data)
-        all_predict_res[str(res)] = test_mae_score
+        all_predict_res[str(res)] = test_mape_score
 
     #找出表现最好（mae最小）的模型
     min_mae = min(all_predict_res.values())
-    print('挑选出最好的模型MAE为 %f' %min_mae)
+    print('挑选出最好的模型误差百分比MAPE为 %f' %min_mae)
     #用表现最好的模型去预测下一日的收盘价
     best_predict_res = list(all_predict_res.keys())[list(all_predict_res.values()).index(min_mae)]
     print('该模型预测的'+inputPd['日期'].loc[inputPd['日期']==new_time].astype(str)+"下"+str(predict_day)+"个交易日的收盘价： "+ str(best_predict_res))
+    print(inputPd['日期'].loc[inputPd['日期']==new_time].astype(str)+"的收盘价： "+ inputPd['沪深300'].loc[inputPd['日期']==new_time].astype(str))
+
 
     #
     # average_mae_history = [np.mean([x[i] for x in all_mae_histories]) for i in range(num_epochs)]
